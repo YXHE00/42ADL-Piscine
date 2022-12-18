1d868691e96db701220f3f55bc039a53036f5850
YXHE00 <105896807+YXHE00@users.noreply.github.com>
2022-12-18 13:24:47 +1030

    This is the subject for the module C 01 of the C Piscine @ 42.
    ex00: OK | ex01: OK | ex02: OK | ex03: OK | ex04: OK | ex05: OK | ex06: OK | ex07: Nothing turned in | ex08: Nothing turned in

---
+++ C01 - 70%/ex01/ft_ultimate_ft.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhe <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 12:03:41 by nhe               #+#    #+#             */
+/*   Updated: 2022/11/30 12:54:08 by nhe              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include<stdio.h>*/
+
+void	ft_ultimate_ft(int	*********nbr)
+{
+	*********nbr = 42;
+}
+/*
+int	main(void)
+{
+	int	a;
+	int	*ptr1;
+	int	**ptr2;
+	int	***ptr3;
+	int	****ptr4;
+	int	*****ptr5;
+	int	******ptr6;
+	int	*******ptr7;
+	int	********ptr8;
+	int	*********ptr9;
+
+	a = 10;
+	ptr1 = &a;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+	printf("Before: a= %d\n", a);
+	ft_ultimate_ft(ptr9);
+	printf("After: a= %d\n", a);
+	return (0);
+}
+*/
